598f422c8e36d8e661aa6c60078151a83aae9b9b
Mohammed Ali <mhalieg@yahoo.com>
2025-02-17 00:20:36 +0200

    add font-size buttons

--- src/Components/ar/SurahViewer.js
+++ src/Components/ar/SurahViewer.js
@@ -129,15 +129,130 @@
 // export default SurahViewer;
 
 
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import  normalizeArabic  from '../../Helper/arabic-normalizer';
+// import surahs from '../../Data/ar/surah-ayahs';
+// import { getSurahDetails, getTypeInArabic } from '../../Helper/Utils';
+
+// const normalizeText = (text) => {
+//   return normalizeArabic(text)
+//     .normalize("NFD") // Normalize to NFD (Canonical Decomposition)
+//     .replace(/[\u0617-\u061A\u064B-\u0652]/g, '') // Remove Tashkeel (diacritics)
+//     .replace(/[\u0640-\u064A]/g, (char) => {
+//       // Replace Arabic letters to unify different forms of the same letter
+//       const map = {
+//         '\u0640': '', '\u0641': 'ف', '\u0642': 'ق', '\u0643': 'ك', '\u0644': 'ل', '\u0645': 'م', '\u0646': 'ن', '\u0647': 'ه', '\u0648': 'و', '\u0649': 'ى', '\u064A': 'ي'
+//       };
+//       return map[char] || char;
+//     });
+// };
+
+
+// const SurahViewer = () => {
+//   const { surahNumber } = useParams();
+//   const [selectedSurah, setSelectedSurah] = useState(null);
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [filteredAyat, setFilteredAyat] = useState([]);
+//   const [selectedAyahIndex, setSelectedAyahIndex] = useState(null); // Track selected Ayah
+//   const handleAyahClick = (index) => {
+//     setSelectedAyahIndex(index); // Set the clicked Ayah as selected
+//   };
+//   useEffect(() => {
+//     const surah = surahs[surahNumber];
+//     //.find(s => s.number.toString() === surahNumber);
+//     if (surah.name) document.title = surah.name;
+//     setSelectedSurah(surah || null);
+//     setFilteredAyat(surah ? surah.ayahs : []);
+//   }, [surahNumber]);
+
+//   useEffect(() => {
+//     if (selectedSurah) {
+//       const filtered = selectedSurah.ayahs.filter(ayah =>
+//         normalizeText(ayah).includes(normalizeText(searchTerm))
+//       );
+//       setFilteredAyat(filtered);
+//     }
+//   }, [searchTerm, selectedSurah]);
+
+//   return (
+//         <>
+//     <style>{`
+//     .ayah-number {
+//     font-weight: 600;
+// }
+// .ayah-wrapper{
+// display: inline;
+// }
+// .ayah-text {
+//   font-size: 1.2rem;
+//   font-family: 'Uthmani', Arial, sans-serif; /* You can use Uthmani font for Mushaf style */
+//   direction: rtl;
+// }
+// .ayah-number{
+// padding-left: 5px;
+// padding-right: 5px;
+// }
+// .highlighted {
+//   background-color: #ffeb3b; /* Yellow background for highlight */
+//   border-radius: 5px;
+//   transition: background-color 0.3s ease-in-out; /* Smooth transition for the highlight */
+// }
+// body {
+//   direction: rtl;
+//   text-align: right;
+// }
+// `}</style>
+//     <div className="container mt-5">
+//       <h1 className="mb-4">عرض آيات السورة</h1>
+//       {selectedSurah ? (
+    
+//         <div>
+//           <div className="list-group">
+//             <div className="list-group-item">
+//               <h5 className="mb-1">سورة {surahNumber}: {getSurahDetails(surahNumber).name}</h5>
+//               <p className="mb-1">نوع السورة: { getTypeInArabic(getSurahDetails(surahNumber).type)}</p>
+//               <p className="mb-1">عدد الآيات: {getSurahDetails(surahNumber).total_verses}</p>
+//             </div>
+//           </div>
+//           <input
+//             type="text"
+//             className="form-control my-4"
+//             placeholder="ابحث في الآيات..."
+//             value={searchTerm}
+//             onChange={(e) => setSearchTerm(e.target.value)}
+//           />
+//           <div className="ayat-container">
+//             {filteredAyat.map((ayah, index) => (
+//               <div key={index}  onClick={() => handleAyahClick(index)}  className={`ayah-wrapper ${
+//                 selectedAyahIndex === index ? 'highlighted' : ''
+//               }`}>
+//                {/* Ayah Text and Number Inline */}
+//             <span className="ayah-text">{ayah.text}</span>
+//             <span className="ayah-number">({index + 1})</span>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       ) : (
+//         <p>السورة غير موجودة.</p>
+//       )}
+//     </div>
+//     </>
+//   );
+// };
+
+// export default SurahViewer;
+
+
+//using global data
+
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import  normalizeArabic  from '../../Helper/arabic-normalizer';
-const surahs = [
-  { number: 1, name: 'ٱلْفَاتِحَة', type: 'Meccan', totalVerses: 7, ayat: ['بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ', 'الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ', 'الرَّحْمَـٰنِ الرَّحِيمِ', 'مَالِكِ يَوْمِ الدِّينِ', 'إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ', 'اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ', 'صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ'] },
-  { number: 2, name: 'ٱلْبَقَرَة', type: 'Medinan', totalVerses: 286, ayat: ['الم', 'ذَٰلِكَ الْكِتَابُ لَا رَيْبَ ۛ فِيهِ هُدًى لِّلْمُتَّقِينَ', 'الَّذِينَ يُؤْمِنُونَ بِالْغَيْبِ وَيُقِيمُونَ الصَّلَاةَ وَمِمَّا رَزَقْنَاهُمْ يُنفِقُونَ', /* add more ayat here */] },
-  { number: 3, name: 'آلِ عِمْرَان', type: 'Medinan', totalVerses: 200, ayat: ['الم', 'اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ', /* add more ayat here */] },
-  // Add more surahs here
-];
+//import surahs from '../../Data/ar/surah-ayahs';
+import { getAllSurahWithAyat, getSurahDetails, getTypeInArabic } from '../../Helper/Utils';
 
 const normalizeText = (text) => {
   return normalizeArabic(text)
@@ -152,37 +267,79 @@ const normalizeText = (text) => {
     });
 };
 
+
 const SurahViewer = () => {
   const { surahNumber } = useParams();
   const [selectedSurah, setSelectedSurah] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredAyat, setFilteredAyat] = useState([]);
-
+  const [selectedAyahIndex, setSelectedAyahIndex] = useState(null); // Track selected Ayah
+  const handleAyahClick = (index) => {
+    setSelectedAyahIndex(index); // Set the clicked Ayah as selected
+  };
   useEffect(() => {
-    const surah = surahs.find(s => s.number.toString() === surahNumber);
+    const surah = getAllSurahWithAyat(surahNumber);
+    //surahs[surahNumber];
+    //.find(s => s.number.toString() === surahNumber);
+    if (surah.name) document.title = surah.name.ar;
     setSelectedSurah(surah || null);
-    setFilteredAyat(surah ? surah.ayat : []);
+    setFilteredAyat(surah ? surah.verses : []);
   }, [surahNumber]);
 
   useEffect(() => {
     if (selectedSurah) {
-      const filtered = selectedSurah.ayat.filter(ayah =>
-        normalizeText(ayah).includes(normalizeText(searchTerm))
+      const filtered = selectedSurah.verses.filter(ayah =>
+        normalizeText(ayah.text.ar).includes(normalizeText(searchTerm))
       );
       setFilteredAyat(filtered);
     }
   }, [searchTerm, selectedSurah]);
 
   return (
+        <>
+    <style>{`
+    .ayah-number {
+    font-weight: 600;
+}
+.ayah-wrapper{
+display: inline;
+}
+.ayah-text {
+  font-size: 1.2rem;
+  font-family: 'Uthmani', Arial, sans-serif; /* You can use Uthmani font for Mushaf style */
+  direction: rtl;
+}
+.ayah-number{
+padding-left: 5px;
+padding-right: 5px;
+}
+.highlighted {
+  background-color: #ffeb3b; /* Yellow background for highlight */
+  border-radius: 5px;
+  transition: background-color 0.3s ease-in-out; /* Smooth transition for the highlight */
+}
+body {
+  direction: rtl;
+  text-align: right;
+}
+`}</style>
     <div className="container mt-5">
       <h1 className="mb-4">عرض آيات السورة</h1>
       {selectedSurah ? (
+    
         <div>
+          
+          {/* Font Size Controls */}
+          <div className="my-4">
+            <button id="increase-font" className="btn btn-primary me-2">تكبير الخط</button>
+            <button id="decrease-font" className="btn btn-secondary">تصغير الخط</button>
+          </div>
+
           <div className="list-group">
             <div className="list-group-item">
-              <h5 className="mb-1">سورة {selectedSurah.number}: {selectedSurah.name}</h5>
-              <p className="mb-1">نوع السورة: {selectedSurah.type === 'Meccan' ? 'مكية' : 'مدنية'}</p>
-              <p className="mb-1">عدد الآيات: {selectedSurah.totalVerses}</p>
+              <h5 className="mb-1">سورة {surahNumber}: {getSurahDetails(surahNumber).name}</h5>
+              <p className="mb-1">نوع السورة: { getTypeInArabic(getSurahDetails(surahNumber).type)}</p>
+              <p className="mb-1">عدد الآيات: {getSurahDetails(surahNumber).total_verses}</p>
             </div>
           </div>
           <input
@@ -192,10 +349,14 @@ const SurahViewer = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <div className="list-group">
+          <div className="ayat-container">
             {filteredAyat.map((ayah, index) => (
-              <div key={index} className="list-group-item">
-                {ayah}
+              <div key={index}  onClick={() => handleAyahClick(index)}  className={`ayah-wrapper ${
+                selectedAyahIndex === index ? 'highlighted' : ''
+              }`}>
+               {/* Ayah Text and Number Inline */}
+            <span className="ayah-text">{ayah.text.ar}</span>
+            <span className="ayah-number">({ayah.number})</span>
               </div>
             ))}
           </div>
@@ -204,6 +365,7 @@ const SurahViewer = () => {
         <p>السورة غير موجودة.</p>
       )}
     </div>
+    </>
   );
 };
 
--- src/Helper/Utils.js
+++ src/Helper/Utils.js
@@ -1,5 +1,7 @@
+import SurahsDetails from '../Data/SurahsDetails';
+import surahs from '../Data/UniversalQuranDB';
 export const getTypeInArabic = (type) => {
-    return type === 'Meccan' ? 'مكية' : 'مدنية';
+    return type.toLowerCase() == 'meccan' ? 'مكية' : 'مدنية';
   };
   export const removeTashkeel = (text) => {
     text = text.replace(/ْ/g, ""); // ْ
@@ -14,3 +16,10 @@ export const getTypeInArabic = (type) => {
     return text.replace(/[\u0617-\u061A\u064B-\u0652]/g, '');
   };
    // export { getTypeInArabic, removeTashkeel };
+
+   export const getSurahDetails = (surahNumber) => {
+    return SurahsDetails[surahNumber - 1];
+  }
+  export const getAllSurahWithAyat = (surahNumber) => {
+    return surahs[surahNumber - 1];
+  }
