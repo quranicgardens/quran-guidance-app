9c541c7ddfa805cca2b110d8cc2ba1b7db81fe48
Mohammed Ali <mhalieg@yahoo.com>
2025-02-21 08:01:11 +0200

    add audio to matn

--- src/Components/Motoon/shatibyah/AllBayts.js
+++ src/Components/Motoon/shatibyah/AllBayts.js
@@ -1,6 +1,8 @@
 // src/components/AllBaytsSearch.js
 import React, { useState } from "react";
 import bayts from "../../../Data/Motoon/Shatibya/Shatibya";
+const audioPath = 'https://ia903005.us.archive.org/18/items/shatibyyah/shatibyyah.mp3'; // Replace with actual path
+
 const AllBayts = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -9,7 +11,27 @@ const AllBayts = () => {
   );
 
   return (
+    <>
     <div className="container mt-3">
+      <h1 className="text-center mt-5">الشاطبية</h1>
+      <h2 className="text-center">بصوت الشيخ طه الفهد</h2>
+    
+      <audio controls autoplay >
+  <source src={audioPath} type="audio/mp3" />
+ 
+Your browser does not support the audio element.
+</audio>
+      {/* Download Button */}
+      <div className="mt-3">
+        <a
+          href={audioPath} // Replace with actual path
+          download
+          className="btn btn-primary"
+        >
+          تحميل الشاطبية بصوت الشيخ طه الفهد
+        </a>
+      </div>
+    
       <h3>Search Bayts in Poem</h3>
       <input
         type="text"
@@ -30,6 +52,7 @@ const AllBayts = () => {
         </ul>
       )}
     </div>
+    </>
   );
 };
 
